a55a855979213fa8e278571ea932b740c984ad43
khiri <abdelohabk@gmail.com>
2023-11-02 12:45:45 +0100

    creating the Testing Class

---
+++ index.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace DVLD_Projet.FRMS_TestsAppoinments
+{
+    public  class Testing
+    {
+        public string name;
+        public string description;
+        public Testing(String Name,String Description)
+        {
+            this.name = Name;
+            this.description = Description;
+        }
+    }
+}
